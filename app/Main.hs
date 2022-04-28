783d9ba949b04c9fc77fa94d8195585894dcde1f
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2022-04-28 21:14:08 +0900

    added unwordsList

--- app/Main.hs
+++ app/Main.hs
@@ -211,6 +211,11 @@ showVal (Atom name) = name
 showVal (Number contents) = show contents
 showVal (Bool True) = "#t"
 showVal (Bool False) = "#f"
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
 
 main :: IO ()
 main = do
